9a02777c1d7df9c91dd0b1dd748e0ca2a954d3dd
Selvin <selvincastillo856@gmail.com>
2025-04-15 09:22:16 -0400

    copy for changing stuff

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,50 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Tabs } from "expo-router";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  return(
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
+    //adding the clickable tabs? and adding the names
+    <Tabs screenOptions={{
+      //icon color for home
+      tabBarActiveTintColor: '#ffd33d',
+      headerStyle: {
+        //background of the header title "home"
+        backgroundColor: '#25292e'
+      },
+      headerShadowVisible: false,
+      // on the home page, it changes the title "home color"
+      headerTintColor: '#fff', 
+      tabBarStyle: {
+      //background of the button icons
+      backgroundColor: '#25292e'
+      }
+    }}>
+
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
+          //name under the icon
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
+          //Icon shape, color, and size
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }} 
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+
+      <Tabs.Screen 
+      //changing style of the about button ^
+        name="about" 
+        options={{ 
+          title: 'About', 
+          tabBarIcon:({ color, focused}) => (
+            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
+          ),
         }}
       />
     </Tabs>
+  
   );
+
 }
